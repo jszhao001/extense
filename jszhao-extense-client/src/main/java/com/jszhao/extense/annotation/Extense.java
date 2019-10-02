8ec142a4344380fbc9bfc2e3597f3ae58fcf321e
jszhao <tongtiandeshu@yeah.net>
2019-10-02 16:53:47 +0800

    init

---
+++ jszhao-extense-client/src/main/java/com/jszhao/extense/annotation/Extense.java
@@ -0,0 +1,7 @@
+package com.jszhao.extense.annotation;
+
+/**
+ * 扩展点
+ */
+public @interface Extense {
+}
---
+++ jszhao-extense-service/src/main/java/com/jszhao/extense/service/Main.java
@@ -0,0 +1,7 @@
+package com.jszhao.extense.service;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Test");
+    }
+}
